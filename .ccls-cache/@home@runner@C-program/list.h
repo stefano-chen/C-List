2c1ce8deccb8ad740ff30bf475beec347b38679b
Stefano Chen <56561038+stefano-chen@users.noreply.github.com>
2022-12-03 16:46:44 +0000

    list

---
+++ .ccls-cache/@home@runner@C-program/list.h
@@ -0,0 +1,67 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Nodo {
+  int val;
+  struct Nodo *next;
+} node;
+
+node *head = NULL;
+
+bool isEmpty() { return (head == NULL); }
+
+int size() {
+  int len = 0;
+  node *p = head;
+  while (p != NULL) {
+    len += 1;
+    p = p->next;
+  }
+  return len;
+}
+
+void append(int val) {
+  if (isEmpty()) {
+    head = (node *)malloc(sizeof(node));
+    head->val = val;
+    head->next = NULL;
+  } else {
+    node *p = head;
+    while (p->next != NULL) {
+      p = p->next;
+    }
+    p->next = (node *)malloc(sizeof(node));
+    p->next->val = val;
+    p->next->next = NULL;
+  }
+}
+
+void destroy() {
+  node *p;
+  while (head != NULL) {
+    p = head;
+    head = head->next;
+    free(p);
+  }
+}
+
+void addTop(int val) {
+  node *p = malloc(sizeof(node));
+  p->val = val;
+  p->next = head;
+  head = p;
+}
+
+void printList() {
+  if (isEmpty()) {
+    printf("Empty List\n");
+  } else {
+    node *p = head;
+    while (p != NULL) {
+      printf("%d ", p->val);
+      p = p->next;
+    }
+    printf("\n");
+  }
+}
--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "list.h"
 
 int main(void) {
-  printf("Hello World\n");
+  for (int i = 0; i < 10; i++) {
+    append(i);
+  }
+  addTop(10);
+  printf("size: %d \n", size());
+  printList();
+  destroy();
+  printf("size: %d \n", size());
+  printList();
   return 0;
 }
